f106843b7ba1a31d80b08e2c380fbbb26b965eff
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2025-08-29 10:22:58 +0200

    feat: restrict CA user to issue certificates only for their org and limit certificate validity

--- src/main/java/com/ftn/bsep/pki/repository/ICertificateRepository.java
+++ src/main/java/com/ftn/bsep/pki/repository/ICertificateRepository.java
@@ -2,8 +2,18 @@ package com.ftn.bsep.pki.repository;
 
 import com.ftn.bsep.pki.entity.Certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ICertificateRepository extends JpaRepository<Certificate, Long> {
+
+    List<Certificate> findAllByOwnerId(Long ownerId);
+
+    @Query("SELECT c FROM Certificate c " +
+            "WHERE c.subjectOrganization = :orgName")
+    List<Certificate> findAllByOrganization(@Param("orgName") String orgName);
 }
--- src/main/java/com/ftn/bsep/pki/service/CertificateService.java
+++ src/main/java/com/ftn/bsep/pki/service/CertificateService.java
@@ -22,7 +22,11 @@ import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import static com.ftn.bsep.pki.entity.RoleName.CA_USER;
 
 @Service
 public class CertificateService {
@@ -104,6 +108,7 @@ public class CertificateService {
         certificateEntity.setRevoked(false);
         certificateEntity.setKeyStorePath(p12.toString());
         certificateEntity.setKeyStorePassword(encryptedPassword);
+        certificateEntity.setOwner(owner);
 
         certificateRepository.save(certificateEntity);
 
@@ -254,6 +259,21 @@ public class CertificateService {
         entity.setRevoked(false);
         entity.setKeyStorePath(p12.toString());
         entity.setKeyStorePassword(encryptedPassword);
+        entity.setOwner(issuerOwner);
+
+        if("CA_USER".equals(issuerOwner.getRole().getName().toString())) {
+            if (!req.organization().equals(issuerOwner.getOrganization())) {
+                throw new RuntimeException("CA user cannot issue certificate for another organization");
+            }
+        }
+
+        // --- Ograničenje trajanja sertifikata ---
+        Instant requestedNotAfter = now.plus(req.validityDays(), ChronoUnit.DAYS);
+        if (requestedNotAfter.isAfter(issuerCertEntity.getNotAfter())) {
+            throw new RuntimeException("Cannot issue certificate that expires after the issuer certificate");
+        }
+
+
 
         System.out.println("✅ Intermediate certificate created successfully: " + entity.getSerialNumber());
 
@@ -285,5 +305,17 @@ public class CertificateService {
                 throw new Exception("Digital signature of the certificate in the chain is invalid: " + currentCert.getSubjectX500Principal().getName() + ". Razlog: " + e.getMessage());
             }
         }
+    }
+
+    public List<Certificate> getCertificatesForUser(User user) {
+        String role = String.valueOf(user.getRole().getName());
+
+        if ("ADMIN".equals(role)) {
+            return certificateRepository.findAll();
+        } else if ("CA_USER".equals(role)) {
+            return certificateRepository.findAllByOrganization(user.getOrganization());
+        }
+
+        return new ArrayList<>();
     }
 }
